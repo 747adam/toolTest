b0f227aa4d519093b7d34b30036d1dc666ddb799
adam.lai[賴奕牧] <adam.lai@104.com.tw>
2020-09-10 14:16:44 +0800

    add pwa

---
+++ pwa/precache-manifest.c1144a3b2c33d6a29742d7f73d3e9386.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d0beb4ac064f5ef2f55",
+    "url": "css/app.67972b85.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/adam.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4045e0d7f8de5807dc5f91ad24b932ad",
+    "url": "index.html"
+  },
+  {
+    "revision": "7d0beb4ac064f5ef2f55",
+    "url": "js/app.c63757ba.js"
+  },
+  {
+    "revision": "9d752fd5dee9ef7e5092",
+    "url": "js/chunk-vendors.7b5ac5b3.js"
+  },
+  {
+    "revision": "7464f9940db934f66acacbab6b0593db",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
